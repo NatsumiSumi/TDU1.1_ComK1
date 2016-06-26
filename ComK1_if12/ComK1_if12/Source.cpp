af411d54377923bc942acb74886cc071f48ae967
Natsumi Sumi <natsumi.sumi0503@gmail.com>
2016-06-27 00:39:28 +0900

    Update ComK1_repeatif1

--- ComK1_if12/ComK1_if12/Source.cpp
+++ ComK1_if12/ComK1_if12/Source.cpp
@@ -7,16 +7,16 @@ int main(void)
 	printf("3つの整数を入力してください。\n");
 	scanf_s("%d%d%d", &num1, &num2, &num3);
 
-	if (num1 <= num2 <= num3 || num2 <= num1 <= num3) {
-		printf("%dが最大値です。", num3);
+	if (num1 >= num2 && num1 >= num3) {
+		printf("%dが最大値です。 \n", num1);
 	}
 
-	else if (num1 <= num3 <= num2 || num3 <= num1 <= num2) {
-		printf("%dが最大値です。", num2);
+	else if (num2 >= num1 && num2 >= num3) {
+		printf("%dが最大値です。 \n", num2);
 	}
 
 	else {
-		printf("%dが最大値です。", num1);
+		printf("%dが最大値です。 \n", num3);
 	}
 
 	return 0;
--- ComK1_if14/ComK1_if14/Source.cpp
+++ ComK1_if14/ComK1_if14/Source.cpp
@@ -8,19 +8,19 @@ int main(void)
 	scanf("%d", &y);
 
 	if (y % 400 == 0) {
-		printf("%dはうるう年です", y);
+		printf("%dはうるう年です \n", y);
 	}
 
 	else if (y % 100 == 0) {
-		printf("%dは平年です", y);
+		printf("%dは平年です \n", y);
 	}
 
 	else if (y % 4 == 0) {
-		printf("%dはうるう年です", y);
+		printf("%dはうるう年です \n", y);
 	}
 
 	else {
-		printf("%dは平年です,y");
+		printf("%dは平年です \n",y);
 	}
 
 	return 0;
