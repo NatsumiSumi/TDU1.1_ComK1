9c76c7e4e62f897da8dac73f2d6e7c4f5e990bcf
Natsumi Sumi <natsumi.sumi0503@gmail.com>
2016-06-27 14:29:41 +0900

    New ComK1_Hairetsu13

---
+++ ComK1_Hairetsu13/ComK1_Hairetsu13/Source.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void) {
+	int mon;
+
+	char eng1[11] = "January";
+	char eng2[11] = "Febuary";
+	char eng3[11] = "March";
+	char eng4[11] = "April";
+	char eng5[11] = "May";
+	char eng6[11] = "June";
+	char eng7[11] = "July";
+	char eng8[11] = "August";
+	char eng9[11] = "September";
+	char eng10[11] = "October";
+	char eng11[11] = "November";
+	char eng12[11] = "December";
+
+	char sto1[16] = "ガーネット";
+	char sto2[16] = "アメジスト";
+	char sto3[16] = "アクアマリン";
+	char sto4[16] = "ダイヤモンド";
+	char sto5[16] = "エメラルド";
+	char sto6[16] = "ムーンストーン";
+	char sto7[16] = "ルビー";
+	char sto8[16] = "ペリドット";
+	char sto9[16] = "サファイヤ";
+	char sto10[16] = "トルマリン";
+	char sto11[16] = "トパーズ";
+	char sto12[16] = "タンザナイト";
+
+	printf("何月ですか =>");
+	scanf_s("%d", &mon);
+
+	printf("%d月のスペルは%sです。 \n",mon,eng
+
+}
