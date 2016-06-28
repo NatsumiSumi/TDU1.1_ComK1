2f2b8901af4bc9b2239f30191bb275601daeb1ba
Natsumi Sumi <natsumi.sumi0503@gmail.com>
2016-06-28 17:40:07 +0900

    New & Done ComK1_Hairetsu15

---
+++ ComK1_Hairetsu15/ComK1_Hairetsu15/Source.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void) {
+	int num[10];
+	int i, j, tmp;
+
+
+	printf("•À‚Ñ‘Ö‚¦‘O: ");
+	for (int i = 0; i < 10; i++) {
+		scanf_s("%d", &num[i]);
+	}
+
+	for (i = 0; i<10; ++i) {
+		printf("%d ", num[i]);
+	}
+	printf("\n");
+
+
+	for (i = 0; i<10; ++i) {
+		for (j = i + 1; j<10; ++j) {
+			if (num[i] < num[j]) {
+				tmp = num[i];
+				num[i] = num[j];
+				num[j] = tmp;
+			}
+		}
+	}
+	
+
+	printf("•À‚Ñ‘Ö‚¦Œã: ");
+	for (i = 0; i<10; ++i){
+		printf("%d ", num[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
