88dff3ae53feb7ce38676558ac7e6f9f6773e5fc
Natsumi Sumi <natsumi.sumi0503@gmail.com>
2016-07-03 07:19:29 +0900

    Rewrite & Done

--- ComK1_Hairetsu12/ComK1_Hairetsu12/Source.cpp
+++ ComK1_Hairetsu12/ComK1_Hairetsu12/Source.cpp
@@ -1,20 +1,31 @@
 #include <stdio.h>
 
 int main(void) {
-	int num[9] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int kuku[9][9];
 	int i;
 
-	for (i = 1; i <= 9; i++) {
-		printf("%2d ", num[0] * i);
-		printf("%2d ", num[1] * i);
-		printf("%2d ", num[2] * i);
-		printf("%2d ", num[3] * i);
-		printf("%2d ", num[4] * i);
-		printf("%2d ", num[5] * i);
-		printf("%2d ", num[6] * i);
-		printf("%2d ", num[7] * i);
-		printf("%2d ", num[8] * i);
+	for (i = 1; i < 10;i++) {
+		kuku[i][0] = i * 1;
+		kuku[i][1] = i * 2;
+		kuku[i][2] = i * 3;
+		kuku[i][3] = i * 4;
+		kuku[i][4] = i * 5;
+		kuku[i][5] = i * 6;
+		kuku[i][6] = i * 7;
+		kuku[i][7] = i * 8;
+		kuku[i][8] = i * 9;
+	}
 
+	for (i = 1; i < 10; i++) {
+		printf("%3d ", kuku[i][0]);
+		printf("%3d ", kuku[i][1]);
+		printf("%3d ", kuku[i][2]);
+		printf("%3d ", kuku[i][3]);
+		printf("%3d ", kuku[i][4]);
+		printf("%3d ", kuku[i][5]);
+		printf("%3d ", kuku[i][6]);
+		printf("%3d ", kuku[i][7]);
+		printf("%3d ", kuku[i][8]);
 		printf("\n");
 	}
 
