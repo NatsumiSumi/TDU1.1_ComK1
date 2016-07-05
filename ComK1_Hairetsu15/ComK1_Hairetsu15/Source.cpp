d03e328b059d0487432e8b1f0f4668e0ef395591
Natsumi Sumi <natsumi.sumi0503@gmail.com>
2016-07-05 20:26:20 +0900

    Update

--- ComK1_Hairetsu15/ComK1_Hairetsu15/Source.cpp
+++ ComK1_Hairetsu15/ComK1_Hairetsu15/Source.cpp
@@ -5,13 +5,13 @@ int main(void) {
 	int i, j, tmp;
 
 
-	printf("•À‚Ñ‘Ö‚¦‘O: ");
+	printf("®”‚ð10ŒÂ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢=> \n");
 	for (int i = 0; i < 10; i++) {
 		scanf_s("%d", &num[i]);
 	}
 
-	for (i = 0; i<10; ++i) {
-		for (j = i + 1; j<10; ++j) {
+	for (i = 0; i < 10; i++) {
+		for (j = i + 1; j < 10; j++) {
 			if (num[i] < num[j]) {
 				tmp = num[i];
 				num[i] = num[j];
