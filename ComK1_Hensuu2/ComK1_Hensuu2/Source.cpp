bc3a697df785bdd721e84295e71a11d90abc7a1b
Natsumi Sumi <natsumi.sumi0503@gmail.com>
2016-08-03 14:44:52 +0900

    Rewrite

--- ComK1_Hensuu2/ComK1_Hensuu2/Source.cpp
+++ ComK1_Hensuu2/ComK1_Hensuu2/Source.cpp
@@ -1,18 +1,30 @@
 #include <stdio.h>
 
+/*自作関数のプロトタイプ宣言*/
+int calcVolume(int, int, int);
+int calcSurface(int, int, int);
+
 int main(void)
 {
-	int num1, num2;
+	int x_length, y_length, z_length;
 
-	printf("三角形の底辺の長さを入力してください。\n");
+	/*3辺の長さの入力*/
+	printf("直方体の3辺の長さx,y,z:");
+	scanf_s("%d", &x_length);
+	scanf_s("%d", &y_length);
+	scanf_s("%d", &z_length);
 
-	scanf("%d", &num1);
-
-	printf("三角形の高さを入力してください。\n");
-
-	scanf("%d", &num2);
-
-	printf("三角形の面積は%dです。\n", num1*num2 / 2);
-
+	/*結果の表示*/
+	printf("立方体の体積は%d、表面積は%d \n", calcVolume(x_length, y_length, z_length), calcSurface(x_length, y_length, z_length));
 	return 0;
+}
+
+/*直方体の体積の計算*/
+int calcVolume(int x, int y, int z) {
+	return x*y*z;
+}
+
+/*直方体の体積の計算*/
+int calcSurface(int x, int y, int z) {
+	return 2 * (x*y + y*z + z*x);
 }
