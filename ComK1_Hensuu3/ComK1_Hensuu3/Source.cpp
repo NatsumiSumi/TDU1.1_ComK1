ca4742afca8592bc68e72487692e5e01aa843d75
Natsumi Sumi <natsumi.sumi0503@gmail.com>
2016-08-22 15:39:41 +0900

    Rewrite&Done

--- ComK1_Hensuu3/ComK1_Hensuu3/Source.cpp
+++ ComK1_Hensuu3/ComK1_Hensuu3/Source.cpp
@@ -1,15 +1,30 @@
 #include <stdio.h>
+
+/*自作関数のプロトタイプ宣言*/
+double calcDirect(double, double);
+double calcParallel(double, double);
+
 int main(void)
 {
-	double num1, num2;
+	double resist1, resist2;
 
-	printf("2つの抵抗値を入力してください。\n");
+	/*抵抗値の入力*/
+	printf("2つの抵抗値を入力してください. \n");
+	scanf("%lf%lf", &resist1, &resist2);
 
-	scanf("%lf%lf", &num1, &num2);
-
-	printf("直列接続の合成抵抗は%.2f (Ω）\n", num1 + num2);
-
-	printf("並列接続の合成抵抗は%.2f (Ω）です", num1*num2 / (num1 + num2));
+	/*結果の表示*/
+	printf("直列接続の合成抵抗は%.2f (Ω）\n", calcDirect(resist1, resist2));
+	printf("並列接続の合成抵抗は%.2f (Ω）です. \n", calcParallel(resist1, resist2));
 
 	return 0;
+}
+
+/*直列接続の合成抵抗の計算*/
+double calcDirect(double x, double y) {
+	return x + y;
+}
+
+/*並列接続の合成抵抗の計算*/
+double calcParallel(double x, double y) {
+	return x*y / (x + y);
 }
