4ccd9d715d0b8075c839ec4cea02da4e22094f19
Natsumi Sumi <natsumi.sumi0503@gmail.com>
2016-08-22 15:39:59 +0900

    Rewrite&Done

--- ComK1_Hensuu4/ComK1_Hensuu4/Source.cpp
+++ ComK1_Hensuu4/ComK1_Hensuu4/Source.cpp
@@ -1,15 +1,30 @@
 #include <stdio.h>
 
+/*自作関数のプロトタイプ宣言*/
+int divideNum_f(int);
+int divideNum_l(int);
+
 int main(void)
 {
 	int num;
 
+	/*整数の入力*/
 	printf("5桁の整数を入力してください。\n");
-
 	scanf("%d", &num);
 
-	printf("上二桁=%d \n", num / 1000);
-	printf("下二桁=%d", num % 1000);
+	/*結果の表示*/
+	printf("上二桁=%d \n", divideNum_f(num));
+	printf("下三桁=%d \n", divideNum_l(num));
 
 	return 0;
+}
+
+/*上二桁を求める*/
+int divideNum_f(int x) {
+	return x / 1000;
+}
+
+/*下三桁を求める*/
+int divideNum_l(int x) {
+	return x % 1000;
 }
